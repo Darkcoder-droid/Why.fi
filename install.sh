61cf1aeb95aa8481e0330ec413a04850effc183f
notysozu <lifegamer2050@gmail.com>
2026-03-28 02:21:50 +0530

    feat(install.sh): add package manager detection

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     DETECTED_OS="macos"
 fi
+PKG_MGR=""
+if command -v apt &> /dev/null; then PKG_MGR="apt-get";
+elif command -v brew &> /dev/null; then PKG_MGR="brew";
+elif command -v dnf &> /dev/null; then PKG_MGR="dnf";
+elif command -v pacman &> /dev/null; then PKG_MGR="pacman";
+fi
